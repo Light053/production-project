805d953ee927be32391c53594285ed63fad925e7
Nicolae <ksirbu519@gmail.com>
2024-01-11 15:47:03 +0200

    change toggle button

--- src/widgets/sidebar/ui/sidebar/sidebar.tsx
+++ src/widgets/sidebar/ui/sidebar/sidebar.tsx
@@ -3,35 +3,44 @@ import styles from './sidebar.module.scss'
 import { FC, PropsWithChildren, useState } from "react"
 import { ThemeSwitcher } from "widgets/theme-switcher";
 import { LangSwitcher } from "widgets/lang-switcher";
+import { useTranslation } from "react-i18next";
+import { Button, ButtonTheme } from "shared/button";
 
 interface SidebarProps extends PropsWithChildren {
 	className?: string;
 }
 
 export const Sidebar: FC<SidebarProps> = (props) => {
-	const [collapsed, setCollapsed] = useState(false);
+	const [collapsed, setCollapsed] = useState(false)
+	const { t } = useTranslation();
 
-	const { className } = props
+	const { className, ...otherProps } = props
 
 	const onToggle = () => {
-		setCollapsed((prev) => !prev);
-	};
+		setCollapsed(prev => !prev)
+	}
 
 	return (
 		<div
-			data-testid="sidebar"
-			className={classNames(styles.Sidebar, { [styles.collapsed]: collapsed }, [className])}
-		>
-			<button
-				data-testid="sidebar-toggle"
+			data-testid='sidebar'
+			className={classNames(
+				styles.sidebar,
+				{ [styles.collapsed]: collapsed },
+				[className])}>
+
+
+			<Button
+				data-testid='sidebar-toggle'
 				onClick={onToggle}
 			>
-				toggle
-			</button>
-			<div className={styles.switchers}>
-				<ThemeSwitcher />
+				{t('Переключить')}
+			</Button>
+
+
+			<div className={classNames(styles.switchers, {}, [])}>
+				<ThemeSwitcher className="" />
 				<LangSwitcher className={styles.lang} />
 			</div>
 		</div>
-	);
+	)
 }
